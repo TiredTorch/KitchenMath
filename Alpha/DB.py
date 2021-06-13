626e63866478f718351f0095f2e523237804b2bf
Tr.Torch <vitochek337@gmail.com>
2021-06-13 23:59:55 +0300

    Start creating fill report system

--- Alpha/DB.py
+++ Alpha/DB.py
@@ -1,92 +1,140 @@
 import openpyxl
 
 wb = openpyxl.reader.excel.load_workbook(filename="DB.xlsx", data_only = True)
+wbT = openpyxl.reader.excel.load_workbook(filename="template.xlsx", data_only = True)
+wb.active = 0  #база данных ингредиентов
+ws0 = wb.active
+wb.active = 1  #база данных блюд
+ws1 = wb.active
+wbT.active = 0  #переменные шаблона
+wsT = wbT.active
 
 def AddIngredient(): 
     print(chr(27) + "[2J")
-    wb.active = 0
-    ws = wb.active
 
     i = 1
-    while(ws['A'+str(i)].value != '*'):
+    while(ws0['A'+str(i)].value != '*'):
         i+=1
     
-    ws['B'+str(i)].value = input('Название ингредиента: ')
-    ws['C'+str(i)].value = float(input('Его цена: '))
-    ws['A'+str(i)].value = int(ws['A'+str(i-1)].value) + 1
-    ws['A'+str(i+1)].value = '*'
+    ws0['B'+str(i)].value = input('Название ингредиента: ')
+    ws0['C'+str(i)].value = float(input('Его цена: '))
+    ws0['A'+str(i)].value = int(ws0['A'+str(i-1)].value) + 1
+    ws0['A'+str(i+1)].value = '*'
     wb.save("DB.xlsx")
 
 def AddDish():    
     print(chr(27) + "[2J")
-    wb.active = 0
-    wsh = wb.active
     
-    wb.active = 1
-    ws = wb.active
-
     i = 1
-    while(ws['H'+str(i)].value != '*'):
+    while(ws1['H'+str(i)].value != '*'):
         i+=1
 
-    ws['A'+str(i)].value = input('Введите название блюда: ')
-    ws['B'+str(i)].value = int(input('Введите количество ингредиентов: '))
-    ws['C'+str(i)].value = ''
+    ws1['A'+str(i)].value = input('Введите название блюда: ')
+    ws1['B'+str(i)].value = int(input('Введите количество ингредиентов: '))
+    ws1['C'+str(i)].value = ''
 
-    ws['A'+str(i+1)].value = 'Продукты'
-    ws['B'+str(i+1)].value = 'Цена за кг'
-    ws['C'+str(i+1)].value = 'Кол-во (сад)'
-    ws['D'+str(i+1)].value = 'Кол-во (ясли)'
-    ws['E'+str(i+1)].value = 'СумСт (сад)'
-    ws['F'+str(i+1)].value = 'СумСт (ясли)'
-    ws['G'+str(i)].value = 'ID:'
+    ws1['A'+str(i+1)].value = 'Продукты'
+    ws1['B'+str(i+1)].value = 'Цена за кг'
+    ws1['C'+str(i+1)].value = 'Кол-во (сад)'
+    ws1['D'+str(i+1)].value = 'Кол-во (ясли)'
+    ws1['E'+str(i+1)].value = 'СумСт (сад)'
+    ws1['F'+str(i+1)].value = 'СумСт (ясли)'
+    ws1['G'+str(i)].value = 'ID:'
 
-    for j in range(ws['B'+str(i)].value):
+    for j in range(ws1['B'+str(i)].value):
         a = int(input('Укажите ID ингредиента '+str(j+1)+': '))
         
-        ws['A'+str(i+2+j)].value = wsh['B'+str(a+2)].value
-        ws['B'+str(i+2+j)].value = wsh['C'+str(a+2)].value
+        ws1['A'+str(i+2+j)].value = ws0['B'+str(a+2)].value
+        ws1['B'+str(i+2+j)].value = ws0['C'+str(a+2)].value
 
-        ws['C'+str(i+2+j)].value = int(input('Введите кол-во для сада: '))
-        ws['D'+str(i+2+j)].value = int(input('Введите кол-во для яслей: '))
+        ws1['C'+str(i+2+j)].value = int(input('Введите кол-во для сада: '))
+        ws1['D'+str(i+2+j)].value = int(input('Введите кол-во для яслей: '))
 
-        ws['E'+str(i+2+j)].value = '=B'+str(i+2+j)+'*C'+str(i+2+j)+'/1000'
-        ws['F'+str(i+2+j)].value = '=B'+str(i+2+j)+'*D'+str(i+2+j)+'/1000'
+        ws1['E'+str(i+2+j)].value = '=B'+str(i+2+j)+'*C'+str(i+2+j)+'/1000'
+        ws1['F'+str(i+2+j)].value = '=B'+str(i+2+j)+'*D'+str(i+2+j)+'/1000'
 
         print(chr(27) + "[2J")
 
-        ws['E'+str(i+3+int(ws['B'+str(i)].value))].value = 'Сумма: '
-        ws['E'+str(i+4+int(ws['B'+str(i)].value))].value = '=SUM(E'+str(i+2)+':E'+str(i+2+j)+')'
-        ws['F'+str(i+4+int(ws['B'+str(i)].value))].value = '=SUM(F'+str(i+2)+':F'+str(i+2+j)+')'
+        ws1['E'+str(i+3+int(ws1['B'+str(i)].value))].value = 'Сумма: '
+        ws1['E'+str(i+4+int(ws1['B'+str(i)].value))].value = '=SUM(E'+str(i+2)+':E'+str(i+2+j)+')'
+        ws1['F'+str(i+4+int(ws1['B'+str(i)].value))].value = '=SUM(F'+str(i+2)+':F'+str(i+2+j)+')'
 
 
-    ws['H'+str(i+6+int(ws['B'+str(i)].value))].value = '*'
+    ws1['H'+str(i+6+int(ws1['B'+str(i)].value))].value = '*'
 
     temp = 1
-    while(ws['H'+str(i-temp)].value == None):
+    while(ws1['H'+str(i-temp)].value == None):
         temp+=1
 
-    ws['H'+str(i)].value = ws['H'+str(i-temp)].value + 1
+    ws1['H'+str(i)].value = ws1['H'+str(i-temp)].value + 1
 
     wb.save("DB.xlsx")
 
 def ShowIngredientID():
     print(chr(27) + "[2J")
-    wb.active = 0
-    ws = wb.active
 
     i = 1
-    while(ws['A'+str(i)].value != '*'):
-        print(ws['B'+str(i)].value + ' :: ' + str(ws['A'+str(i)].value))
+    while(ws0['A'+str(i)].value != '*'):
+        print(ws0['B'+str(i)].value + ' :: ' + str(ws0['A'+str(i)].value))
         i+=1
 
 def ShowDishesID():
     print(chr(27) + "[2J")
-    wb.active = 1
-    ws = wb.active
 
     print('Блюдо :: ID')
     i = 2
-    while(ws['H'+str(i)].value != '*'):
-        print(ws['A'+str(i)].value + ' :: ' + str(ws['H'+str(i)].value))
-        i+=(ws['B'+str(i)].value + 6)
+    while(ws1['H'+str(i)].value != '*'):
+        print(ws1['A'+str(i)].value + ' :: ' + str(ws1['H'+str(i)].value))
+        i+=(ws1['B'+str(i)].value + 6)
+
+def FillReport():
+
+
+    letters = ['D','F','H','J','L','N']
+    PrepareTable(letters)
+
+    for FT in range (3):
+        print(chr(27) + "[2J")
+        dishesAmount = int(input('Сколько блюд в ' + str(FT + 1) + ' приеме пищи: '))
+        for DT in range (dishesAmount):
+            DishPars(str(letters[FT * 2]), str(letters[(FT * 2) + 1]))
+
+    
+    wbT.save("DBA.xlsx")
+
+def DishPars(firCell, secCell):
+
+    tempDishID = int(input('Введите ID блюда: '))
+
+    wsT[str(firCell)+'17'].value += (input('Введите его количество: ') + '\n')
+
+    pointDB = 1
+    while(tempDishID != ws1['H'+str(pointDB)].value): #ищет блюдо парсом по айди
+        pointDB+=1
+    
+    wsT[firCell+'11'].value += (ws1['A'+str(pointDB)].value + '\n')
+    IngrPars(pointDB, firCell, secCell)
+
+
+def IngrPars(pointDB, firCell, secCell):
+    for k in range(ws1['B'+str(pointDB)].value): #для каждого ингр
+        pointRep = 19
+        while(wsT['A'+str(pointRep)].value != ws1['A'+str(pointDB + 2 + k)].value): #ищет ингр в отчете
+            pointRep+=1
+        if(wsT[str(firCell)+str(pointRep)].value): #заполнение сумст
+            wsT[str(secCell)+str(pointRep)].value += float(ws1['E'+str(pointDB + 2 + k)].value)
+        else:
+            wsT[str(firCell)+str(pointRep)].value = ws1['E'+str(pointDB + 2 + k)].value 
+
+def PrepareTable(letters):
+    wsT['D17'].value = str('')
+    wsT['D11'].value = str('')
+    wsT['H17'].value = str('')
+    wsT['H11'].value = str('')
+    wsT['L17'].value = str('')
+    wsT['L11'].value = str('')
+    for letter in letters:
+        for num in range (19, 81):
+            wsT[letter+str(num)].value = float()
+
+FillReport()
--- Demo/Texts.py
+++ Demo/Texts.py
@@ -1,13 +1,24 @@
 import openpyxl
+from openpyxl.styles import Alignment
 
 wb = openpyxl.reader.excel.load_workbook(filename="sample.xlsx", data_only = True)
 
 wb.active = 0
 ws = wb.active
 
-find = input()
+def Test():
 
-for i in range(2, 11):
-    if(str(find) == str(ws['A'+str(i)].value)):
-        print("Успех")
-        break
+    #ws['F2'].value = "ff\nff"
+    #ws['F2'].value += "aaaa"
+
+    #ws['F2'].alignment = Alignment(text_rotation=90, horizontal='center')
+
+
+    if(ws['F2'].value):
+        print(1)
+    else:
+        print(0)
+
+    wb.save('sample.xlsx')
+
+Test()
